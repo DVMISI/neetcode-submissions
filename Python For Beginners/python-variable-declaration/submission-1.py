e1bb443c1ed9448016f0cd94966c2dfb20b01ac1
DVMISI <271773683+DVMISI@users.noreply.github.com>
2026-06-11 00:34:41 +0100

    Add: python-variable-declaration - submission-1

---
+++ Python For Beginners/python-variable-declaration/submission-1.py
@@ -0,0 +1,7 @@
+message = "this string is stored in a variable"
+# don't modify the code below this line
+print(message)
+print(message)
+#This way, we can print the same string multiple times without having to retype it.
+
+#Variables are like containers that hold values.$0
